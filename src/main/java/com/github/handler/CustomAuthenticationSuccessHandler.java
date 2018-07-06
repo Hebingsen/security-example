f4ccb0dadefb05bb0a06db86e3ec83915b60d57f
hebingsen <hebingsen@comein.cn>
2018-07-06 17:48:59 +0800

    update

--- src/main/java/com/github/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/github/handler/CustomAuthenticationSuccessHandler.java
@@ -5,17 +5,11 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.core.ResponseResult;
-import com.github.core.ResponseResult.ResponseResultBuilder;
-
 import lombok.extern.slf4j.Slf4j;
 
 /**
